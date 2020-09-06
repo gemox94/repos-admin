e64bd9ff27925a0d15ac63215e55540df4f33f87
Gerardo Moxca Ruiz <gerardo.mx.rz@gmail.com>
2020-09-06 15:20:26 -0500

    configured site layout to get user from store

--- components/SiteLayout.js
+++ components/SiteLayout.js
@@ -1,7 +1,12 @@
-import React from 'react';
-import {GitHub} from 'react-feather';
+import React, { useContext } from 'react';
+
+import { UserContext } from '../context/UserContextProvider';
+import Link from 'next/link';
 
 const SiteLayout = ({ children }) => {
+
+    const [{ user }] = useContext(UserContext);
+
     return(
         <>
             <nav className="level px-5 py-3 has-background-primary">
@@ -10,11 +15,21 @@ const SiteLayout = ({ children }) => {
                     <div className="level-item">
                         <img src="/repos-admin.png" alt="Repos Admin" width="80px" height="80px"/>
                     </div>
+                    <div className="level-item">
+                        <Link href="/private-repos">
+                            <a className="button is-primary">Private repos</a>
+                        </Link>
+                    </div>
+                    <div className="level-item">
+                        <Link href="/public-repos">
+                            <a className="button is-primary">Public repos</a>
+                        </Link>
+                    </div>
                 </div>
 
-                <div className="left-right">
-                    <div className="level-item">
-                        <GitHub />
+                <div className="level-right">
+                    <div className="level-item has-text-white">
+                        <span>{(user && user.name) || ''}</span>
                     </div>
                 </div>
 
